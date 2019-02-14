201a2dbd0e5934e42585ea26b24f6836fece40c5
James Ostergaard <josterga@uci.edu>
2019-02-13 17:55:24 -0800

    testing php form

---
+++ php/signup.php
@@ -0,0 +1,22 @@
+<?php
+/*Connect using SQL Server authentication.*/
+$serverName = "tcp:theatrenow.database.windows.net,1433";
+$connectionOptions = array("Database"=>"TheatreNow",
+                           "UID"=>"grapesandbananas",
+                           "PWD" => "Apples&Oranges");
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+if($conn === false)
+{
+    die(print_r(sqlsrv_errors(), true));
+}
+$email = $_POST["email"];
+$username = $_POST["username"];
+$password = $_POST["password"];
+$type = $_POST["type"];
+
+echo "PHP CODE";
+echo $email;
+echo $username;
+echo $password;
+echo $type;
+?>
