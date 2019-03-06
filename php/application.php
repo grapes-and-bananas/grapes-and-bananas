2dea6950166e6dc12081a75701e980cc27e1950f
James Ostergaard <josterga@uci.edu>
2019-03-05 19:48:27 -0800

    application form and accompanying php

--- php/application.php
+++ php/application.php
@@ -23,7 +23,7 @@ $income = $_POST["income"];
 $comment = $_POST["comment"];
 $agree = $_POST["agree"];+
 
-$tsql = "INSERT INTO Contact
+$tsql = "INSERT INTO Application
 VALUES ('$name', '$email', '$phone', '$venue', '$seat', '$ticket', '$weeks', '$fqcy', '$revenue', '$income', '$comment')";
 
 $getResults= sqlsrv_query($conn, $tsql);
